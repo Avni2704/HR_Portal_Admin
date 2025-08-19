daeabf727c0916bae58fba87c456ebddc13a044a
nini <aininafisah@altecflex.com>
2025-08-19 10:29:10 +0800

    HR Portal Admin Login and Change Password Module 19.08.2025 by Aini

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -4,12 +4,13 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
+        //features = {"src/test/java/features/AdminChangePassword.feature"},
         features = {"src/test/java/features/AdminLogin.feature"},
         //dryRun = true,
         glue = { "steps", "hooks" },
         snippets = CucumberOptions.SnippetType.CAMELCASE,
         monochrome = true, // TO REMOVE JUNK CHARACTERS
-        //tags = "@failed",
+        //tags = "@TC002",
         plugin = { "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" })
 
 public class TestRunner extends AbstractTestNGCucumberTests {
